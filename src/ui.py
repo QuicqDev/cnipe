7880ddcaedf4630b645c7747b1eef68c295f2893
ASH1998 <mishra06ashutosh@gmail.com>
2023-02-06 14:08:11 +0530

    refactor: convert tk app to class for modularity

--- src/ui.py
+++ src/ui.py
@@ -6,8 +6,6 @@ import os
 import tkinter
 import customtkinter
 
-from napper import sc_taker
-
 from PyQt5 import QtWidgets
 from PIL import Image
 
@@ -16,63 +14,64 @@ from html_to_image import make_gradient_image
 from image_shaper import ImgModifier
 
 
-dir_path = os.path.join("..", "temp")
-capture_name = "capture.png"
-gradient_name = "gradient.png"
-
-
-dir_path = os.path.join("..", "temp")
-capture_name = "capture.png"
-gradient_name = "gradient.png"
-
-# customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
-APP = customtkinter.CTk()  # create CTk window like you do with the Tk window
-APP.geometry("800x500")
-APP.wm_iconbitmap("../ui/gnapper.ico")
-APP.iconbitmap(default='../ui/gnapper.ico')
-APP.title("Cnipe - Take Beautiful Screenshots")
 
-
-def button_function():
+class App(customtkinter.CTk):
     """
-    nope
+    Tkinter Class
     """
-    # minimise the app after taking screenshot
-    APP.wm_state('iconic')
-    app = QtWidgets.QApplication([])
-    window = MyWidget(capture_path=os.path.join(dir_path, capture_name))
-    window.show()
-    app.aboutToQuit.connect(app.deleteLater)
-    app.exec_()
-    window.destroy()
 
-    # create gradient
-    gradient_path = make_gradient_image(store_dir=dir_path, gradient_name=gradient_name)
+    def __init__(self):
+        super().__init__()
+        self.geometry("800x500")
+        self.wm_iconbitmap("../ui/gnapper.ico")
+        self.iconbitmap(default='../ui/gnapper.ico')
+        self.title("Cnipe - Take Beautiful Screenshot")
 
-    image_modify_object = ImgModifier(
-        outer_img=Image.open(os.path.join(gradient_path)),
-        inner_img=Image.open(os.path.join(dir_path, capture_name)),
-        padding=25
-    )
+        # place button
+        button = customtkinter.CTkButton(self, text="Capture Screenshot", command=self.capture_screenshot)
+        button.place(relx=0.7, rely=0.5, anchor=tkinter.W)
 
-    modified_image = image_modify_object.paste_img(rounded_corners=16)
-    modified_image.save(os.path.join(dir_path, "modified.png"))
+    def capture_screenshot(self):
+        """
+        capture screenshot
+        """
+        dir_path = os.path.join("..", "temp")
+        capture_name = "capture.png"
+        gradient_name = "gradient.png"
 
-    bg_image = customtkinter.CTkImage(Image.open(os.path.join(dir_path, "modified.png")), size=(300, 400))
-    bg_image_label = customtkinter.CTkLabel(master=APP, image=bg_image)
-    bg_image_label.grid(row=0, column=0)
+        # minimise the app after taking screenshot
+        self.wm_state('iconic')
+        qt_app = QtWidgets.QApplication([])
+        window = MyWidget(capture_path=os.path.join(dir_path, capture_name))
+        window.show()
+        qt_app.aboutToQuit.connect(qt_app.deleteLater)
+        qt_app.exec_()
+        window.destroy()
 
-    # open the app after taking screenshot
-    # APP.wm_state("zoomed")
-    APP.deiconify()
+        # create gradient
+        gradient_path = make_gradient_image(store_dir=dir_path, gradient_name=gradient_name)
 
+        image_modify_object = ImgModifier(
+            outer_img=Image.open(os.path.join(gradient_path)),
+            inner_img=Image.open(os.path.join(dir_path, capture_name)),
+            padding=25
+        )
 
-# Use CTkButton
-button = customtkinter.CTkButton(master=APP, text="Capture Screenshot", command=button_function)
-button.place(relx=0.7, rely=0.5, anchor=tkinter.W)
+        modified_image = image_modify_object.paste_img(rounded_corners=16)
+        modified_image.save(os.path.join(dir_path, "modified.png"))
+
+        bg_image = customtkinter.CTkImage(Image.open(os.path.join(dir_path, "modified.png")), size=(300, 400))
+        bg_image_label = customtkinter.CTkLabel(self, image=bg_image)
+        bg_image_label.grid(row=0, column=0)
+
+        # open the app after taking screenshot
+        # APP.wm_state("zoomed")
+        self.deiconify()
 
 
 if __name__ == "__main__":
-    APP.mainloop()
+    app = App()
+    app.mainloop()
