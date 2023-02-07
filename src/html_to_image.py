93b44dd37f630e2aa74c69571f215722a97a0009
ASH1998 <mishra06ashutosh@gmail.com>
2023-02-07 12:51:11 +0530

    feat: get multiple gradients, use combination of 2 colors or 3 colors in random

--- src/html_to_image.py
+++ src/html_to_image.py
@@ -3,26 +3,42 @@ HTML 2 Image
 @author : Ashutosh | created on : 23-12-2022
 """
 import os
+import random
+
 from html2image import Html2Image
 
 
-def make_gradient_image(store_dir, gradient_name):
+def make_gradient_image(store_dir, gradient_name, colors):
     """
     Make gradients
     :param gradient_name:
     :param store_dir:
+    :param colors:
     :return:
     """
-    hti = Html2Image(output_path=store_dir)
+    if colors is None:
+        colors = list()
+        for _ in range(3):
+            colors.append((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), 1))
 
-    html = """<!DOCTYPE html>
+    hti = Html2Image(output_path=store_dir)
+    degree = random.randint(45, 360)
+
+    grad_strings = [
+        f"{degree}deg,  rgba{colors[0]} 11.2%, rgba{colors[1]} 57.8%, rgba{colors[2]} 85.9%",
+        f"{degree}deg,  rgba{colors[0]} 11.2%, rgba{colors[1]} 57.8%",
+        f"{degree}deg,  rgba{colors[0]} 11.2%, rgba{colors[1]} 57.8%, rgba{colors[2]} 11.9%"
+    ]
+    grad_strings = random.choice(grad_strings)
+
+    html = f"""<!DOCTYPE html>
     <html>
     <head>
     <style> 
-    #grad1 {
+    #grad1 {{
        height: 1080px; width: 1920px;
-      background-image: linear-gradient( 189.6deg,  rgba(247,253,166,1) 11.2%, rgba(128,255,221,1) 57.8%, rgba(255,128,249,1) 85.9% );
-    }
+      background-image: linear-gradient({grad_strings});
+    }}
     </style>
     </head>
     <body>
@@ -34,4 +50,4 @@ def make_gradient_image(store_dir, gradient_name):
 
     hti.screenshot(html_str=html, save_as=gradient_name)
 
-    return os.path.join(store_dir, gradient_name)
+    return os.path.join(store_dir, gradient_name), colors
